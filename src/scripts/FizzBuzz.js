a8791b3030aad09d63fede1daa1c9d0349ca7162
unknown <info@johnnyramirez.es>
2022-11-22 11:45:44 +0100

    Añadido should return Buzz is divisible by five

---
+++ src/scripts/FizzBuzz.js
@@ -0,0 +1,9 @@
+export function fizzbuzz(numb) {
+    if(numb % 3 == 0) {
+        return "Fizz";
+    }
+
+    if(numb % 5 == 0) {
+        return "Buzz";
+    }
+}
--- tests/FizzBuzz.test.js
+++ tests/FizzBuzz.test.js
@@ -1,5 +1,21 @@
+import { fizzbuzz } from "../src/scripts/FizzBuzz";
+
 describe('FizzBuzz', () => {
-    test('should return Fizz', () => {
-        
+    test('should return Fizz is divisible by three', () => {
+        // Given
+        const numb = 3;
+        // When
+        const result = fizzbuzz(numb);
+        // Then
+        expect(result).toBe("Fizz");
+    });
+
+    test('should return Buzz is divisible by five', () => {
+        // Given
+        const numb = 10;
+        // When
+        const result = fizzbuzz(numb);
+        // Then
+        expect(result).toBe("Buzz");
     });
 });
